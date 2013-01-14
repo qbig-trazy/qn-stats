33e11e618a3f19a6fa3ae36fb67cbedf1111b5e8
qbig-trazy <trazy@qbigstudio.com>
2013-01-14 19:06:27 +0900

    working commit

---
+++ application/views/main.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<title>QN-STATS</title>
+	<link rel='stylesheet' type='text/css' media='all' href="/static/css/html5.stylesheet.reset.css" />
+	<link rel='stylesheet' type='text/css' media='all' href="/static/css/base.css" />
+	<link rel='stylesheet' type='text/css' media='all' href="/static/css/tree.menu.css" />
+</head>
+<body>
+
+<header class="block">
+	<h1>QN-STATISTICS</h1>
+</header>
+
+<section class="block">
+	<nav>
+		<ul class="tree">
+			<li>
+				<label for="folder1">Folder 1</label> <input type="checkbox" checked disabled id="folder1" />
+				<ul>
+					<li><a href="#">MENU01</a></li>
+					<li>
+						<label for="subfolder1">Subfolder 1</label> <input type="checkbox" id="subfolder1" /> 
+						<ul>
+							<li><a href="#">MENU01</a></li>
+							<li><a href="#">MENU02</a></li>
+							<li><a href="#">MENU03</a></li>
+						</ul>
+					</li>
+					<li><a href="#">MENU02</a></li>
+					<li><a href="#">MENU03</a></li>
+					<li><a href="#">MENU04</a></li>
+					<li><a href="#">MENU05</a></li>
+				</ul>
+			</li>
+			<li>
+				<label for="folder1">Folder 1</label> <input type="checkbox" checked disabled id="folder1" />
+				<ul>
+					<li><a href="#">MENU01</a></li>
+					<li><a href="#">MENU02</a></li>
+					<li><a href="#">MENU03</a></li>
+					<li><a href="#">MENU04</a></li>
+					<li><a href="#">MENU05</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</section>
+
+<footer class="block">
+	<p>Qbigstudio 2012 ⓒ.</p>
+</footer>
+
+</body>
+</html>
